39a458f826db6f51249b6139ad59bde228d957c7
Nishthakumari <knishtha0@gmail.com>
2023-05-31 13:47:14 +0530

    Hidden Form Field in java servlet

--- Youtube1/src/java/Servlet1.java
+++ Youtube1/src/java/Servlet1.java
@@ -39,7 +39,13 @@ public class Servlet1 extends HttpServlet {
             
             String name =request.getParameter("user_name");
             out.println("<h1> Your name is  "+name+"</h1>");
-            out.println("<a href ='servlet2?user="+name+"'> Go to second servlet</a>");
+            out.println(""
+                    + "<form action = 'servlet2' >"
+                    + "<input type='hidden' name='user_name' value='"+name+"' />"
+                    + "<button> Go to second servlet </button>"
+                    + ""
+                    + ""
+                    + "</form>");
             
             
             out.println("</body>");
